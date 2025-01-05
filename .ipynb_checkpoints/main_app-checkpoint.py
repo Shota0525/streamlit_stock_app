57d5642f4ae4244f113fe072e75c0d8095fec24b
Shota0525 <msg22623892@gmail.com>
2025-01-05 19:33:49 +0900

    アプリのアップデート

--- .ipynb_checkpoints/main_app-checkpoint.py
+++ .ipynb_checkpoints/main_app-checkpoint.py
@@ -9,8 +9,7 @@ from ta.momentum import RSIIndicator
 from ta.volatility import BollingerBands
 
 
-
-# 各関数を定義
+# 各関数を定義 #################################################################################################################################
 # 株価を取得する関数
 def get_stock_price(ticker, period, interval):
     data = yf.download(ticker, period = period, interval = interval)
@@ -22,12 +21,11 @@ def plot_stock_price(ticker, period, title):
     data = get_stock_price(ticker, period = period, interval = '1d')
     # プロットの区切りを設定
     fig = make_subplots(rows=1, cols=1, shared_xaxes=True, vertical_spacing=0.03)
-
     # 株価データと移動曲線を描画
     fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='original'))
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'].rolling(window=25).mean(), name='MA25', line=dict(color='lightcoral')))
     fig.add_trace(go.Scatter(x=data.index, y=data['Close'].rolling(window=50).mean(), name='MA50', line=dict(color='lightblue')))
     fig.add_trace(go.Scatter(x=data.index, y=data['Close'].rolling(window=75).mean(), name='MA75', line=dict(color='lightsalmon')))
-
     # ボリンジャーバンドを描画
     indicator_bb = BollingerBands(close=data["Close"], window=20, window_dev=2)
     fig.add_trace(go.Scatter(x=data.index, y=indicator_bb.bollinger_hband(), name='Upper BB', line=dict(color='palevioletred', dash='dash')))
@@ -37,23 +35,49 @@ def plot_stock_price(ticker, period, title):
     return fig
 
 
+# VIXを描画する関数
+def plot_vix(period):
+    data = get_stock_price('^VIX', period = period, interval = '1d')
+    # プロットの区切りを設定
+    fig = make_subplots(rows=1, cols=1, shared_xaxes=True, vertical_spacing=0.03)
+    # 指数データと移動曲線を描画
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='original'))
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'].rolling(window=25).mean(), name='MA25', line=dict(color='lightcoral')))
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'].rolling(window=50).mean(), name='MA50', line=dict(color='lightblue')))
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'].rolling(window=75).mean(), name='MA75', line=dict(color='lightsalmon')))
+    # 20と30の水平線を追加 
+    fig.add_shape(type='line', x0=data.index[0], x1=data.index[-1], y0=20, y1=20, line=dict(color='palevioletred', width=2, dash='dash')) 
+    fig.add_shape(type='line', x0=data.index[0], x1=data.index[-1], y0=30, y1=30, line=dict(color='red', width=2, dash='dash'))
 
-# アプリ画面構成
+    fig.update_layout(title={'text': 'VIX指数', 'x': 0.5, 'y': 0.8, 'xanchor': 'center', 'yanchor': 'top'}, xaxis_rangeslider_visible=False, showlegend=False)
+    return fig
+
+
+
+###############################################################################################################################################
+
+
+# アプリ画面構成 #######################################################################
 period_list = ['6mo', '1y', '2y']
 period = st.selectbox('表示期間', period_list)
 st.divider()
 
 # 株価指標を表示
-st.title("株価指数")
-col1, col2, col3 = st.columns(3)
+st.title("株価関連指数")
+col1, col2 = st.columns(2)
 with col1:
     st.plotly_chart(plot_stock_price('^DJI', period, 'ダウ平均'))
-    st.plotly_chart(plot_stock_price('^N225', period, '日経平均株価'))
+    st.plotly_chart(plot_stock_price('^GSPC', period, 'S&P500指数'))
 with col2:
     st.plotly_chart(plot_stock_price('^IXIC', period, 'ナスダック総合指数'))
-    st.plotly_chart(plot_stock_price('^VIX', period, 'VIX指数'))
-with col3:
-    st.plotly_chart(plot_stock_price('^GSPC', period, 'S&P500指数'))   
+    st.plotly_chart(plot_stock_price('^N225', period, '日経平均株価'))
+    
+col1, col2 = st.columns(2)
+with col1:
+    st.plotly_chart(plot_vix(period))
+    st.plotly_chart(plot_stock_price('^TNX', period, '米10年国債'))
+with col2:
+    st.plotly_chart(plot_stock_price('^SOX', period, 'フィラデルフィア半導体指数（SOX）'))
 st.divider()
 
 
@@ -71,11 +95,11 @@ st.divider()
 st.title("グローバルサウス（ ETF ）")
 col1, col2 = st.columns(2)
 with col1:
-    st.plotly_chart(plot_stock_price('EPI', period, 'インド'))
-    st.plotly_chart(plot_stock_price('VNM', period, 'ベトナム'))
+    st.plotly_chart(plot_stock_price('EPI', period, 'インド（EPI）'))
+    st.plotly_chart(plot_stock_price('VNM', period, 'ベトナム（VNM）'))
 with col2:
-    st.plotly_chart(plot_stock_price('TUR', period, 'トルコ'))
-    st.plotly_chart(plot_stock_price('EWW', period, 'メキシコ'))
+    st.plotly_chart(plot_stock_price('TUR', period, 'トルコ（TUR）'))
+    st.plotly_chart(plot_stock_price('EWW', period, 'メキシコ（EWW）'))
 st.divider()
 
 
@@ -87,7 +111,7 @@ with col1:
 with col2:
     st.plotly_chart(plot_stock_price('ETH-USD', period, 'イーサリアム（ USD ）'))
 
-
+#######################################################################################
 
 
 
---
+++ pages/.ipynb_checkpoints/01_相場分析-checkpoint.py
@@ -0,0 +1,82 @@
+# ライブラリをインポート
+import streamlit as st
+import yfinance as yf
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import ta
+from ta.momentum import RSIIndicator
+from ta.volatility import BollingerBands
+
+
+# 株価を取得したい銘柄を指定
+################
+ticker = '^N225'
+period = '1y'
+interval = '1d'
+################
+
+
+# 各関数を定義 #################################################################################################################################
+# 株価を取得する関数
+def get_stock_price(ticker, period, interval):
+    data = yf.download(ticker, period = period, interval = interval)
+    return data
+
+
+# 株価データを描画する関数
+def plot_stock_price(ticker, period, interval, title):
+    data = get_stock_price(ticker, period = period, interval = interval)
+    # プロットの区切りを設定
+    fig = make_subplots(rows=1, cols=1, shared_xaxes=True, vertical_spacing=0.03)
+
+    # 株価データと移動曲線を描画
+    fig.add_trace(go.Candlestick(x=data.index, open=data['Open'], high=data['High'], low=data['Low'], close=data['Close'], name='original'))
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'].rolling(window=25).mean(), name='MA25', line=dict(color='lightcoral')))
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'].rolling(window=50).mean(), name='MA50', line=dict(color='lightblue')))
+    fig.add_trace(go.Scatter(x=data.index, y=data['Close'].rolling(window=75).mean(), name='MA75', line=dict(color='lightsalmon')))
+
+    # ボリンジャーバンドも描画
+    indicator_bb = BollingerBands(close=data["Close"], window=20, window_dev=2)
+    fig.add_trace(go.Scatter(x=data.index, y=indicator_bb.bollinger_hband(), name='Upper BB', line=dict(color='palevioletred', dash='dash')))
+    fig.add_trace(go.Scatter(x=data.index, y=indicator_bb.bollinger_lband(), name='Lower BB', line=dict(color='palevioletred', dash='dash')))
+
+    fig.update_layout(title={'text': title, 'x': 0.5, 'y': 0.8, 'xanchor': 'center', 'yanchor': 'top'}, xaxis_rangeslider_visible=False, showlegend=False)
+    return fig
+
+
+# RSIを描画する関数
+def plot_stock_rsi(ticker, period, interval):
+    data = get_stock_price(ticker, period = period, interval = interval)
+    # プロットの区切りを設定
+    fig = make_subplots(rows=1, cols=1, shared_xaxes=True, vertical_spacing=0.03)
+    # RSIを描画
+    rsi = RSIIndicator(data['Close']).rsi()
+    fig.add_trace(go.Scatter(x=data.index, y=rsi, name='RSI', line=dict(color='rosybrown')))
+    # 70と30の水平線を追加 
+    fig.add_shape(type='line', x0=data.index[0], x1=data.index[-1], y0=70, y1=70, line=dict(color='palevioletred', width=2, dash='dash')) 
+    fig.add_shape(type='line', x0=data.index[0], x1=data.index[-1], y0=30, y1=30, line=dict(color='palevioletred', width=2, dash='dash'))
+
+    fig.update_layout(title={'text': 'RSI：Relative Strength Index（相対力指数）', 'x': 0.5, 'y': 0.8, 'xanchor': 'center', 'yanchor': 'top'}, xaxis_rangeslider_visible=False, showlegend=False)
+    return fig
+
+
+# 直近RSIを計算する関数
+def calculate_rsi(ticker, period, interval):
+    data = get_stock_price(ticker, period = period, interval = interval)
+    rsi = RSIIndicator(data['Close']).rsi()
+    latest_rsi = rsi.iloc[-1]
+    return latest_rsi
+
+###############################################################################################################################################
+
+
+# アプリ画面構成 #######################################################################
+# StreamlitでPlotlyグラフを表示
+st.plotly_chart(plot_stock_price(ticker, period, interval, '日経平均株価'))
+st.divider()
+
+# RSIグラフを表示
+st.plotly_chart(plot_stock_rsi(ticker, period, interval))
+st.metric('現在のRSI', "{:,.1f}".format(calculate_rsi(ticker, period, interval)))
+#######################################################################################
